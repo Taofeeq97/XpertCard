ce9405eb0bd76f1d6bc99310e4d6cfe0c78a8516
Your Name <otutaofeeqi@gmail.com>
2023-07-06 20:34:49 +0100

    few updats

--- Expertcard/settings.py
+++ Expertcard/settings.py
@@ -31,8 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-9e1e.up.railway.app', 'http://127.0.0.1']
-CORS_ALLOWED_ORIGINS = ['https://web-production-9e1e.up.railway.app', 'http://127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-5804.up.railway.app/', 'http://127.0.0.1']
+CORS_ALLOWED_ORIGINS = ['https://web-production-5804.up.railway.app', 'http://127.0.0.1']
 
 AUTH_USER_MODEL = 'admin_account.CustomAdminUser'
 DATABASE_URL = os.getenv('DATABASE_URL')
