8fb84473a7661f03f3a4a5dbbd245401d80557b0
Your Name <otutaofeeqi@gmail.com>
2023-07-06 10:08:28 +0100

    allowed origins seettings

--- Expertcard/settings.py
+++ Expertcard/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://web-production-9e1e.up.railway.app', 'http://127.0.0.1:8000','http://127.0.0.1:3000']
+ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://web-production-9e1e.up.railway.app', 'http://127.0.0.1:8000', 'http://127.0.0.1:3000']
 CORS_ALLOWED_ORIGINS = ['https://web-production-9e1e.up.railway.app', 'http://127.0.0.1:8000', 'http://127.0.0.1:3000']
 
