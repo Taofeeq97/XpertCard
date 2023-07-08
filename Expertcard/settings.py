4f43b34fc82db10b7ae5786fd448fa42e6cc27af
Your Name <otutaofeeqi@gmail.com>
2023-07-08 18:44:17 +0100

    allowed cors

--- Expertcard/settings.py
+++ Expertcard/settings.py
@@ -76,7 +76,7 @@ CORS_ORIGIN_WHITELIST = ('https://web-production-5804.up.railway.app',
                          'http://127.0.0.1', 'http://127.0.0.1:5173',
                         'http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'http://localhost:3000')
 
-CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_ALLOW_ALL = True
 
 
 
