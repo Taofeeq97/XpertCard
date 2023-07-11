066060c3f91fb06c0f2a00f5031c2f396c2284d9
Your Name <otutaofeeqi@gmail.com>
2023-07-11 20:34:06 +0100

    some func

--- card/main.py
+++ card/main.py
@@ -1,38 +1,19 @@
 from elasticsearch import Elasticsearch
 
-es = Elasticsearch(['http://localhost:9200'], http_auth=('elastic', '7PeMZZH7haepPZTxMBpP'))
+client = Elasticsearch(
+    hosts=['http://localhost:9200'],
+    http_auth=('elastic', '7PeMZZH7haepPZTxMBpP')
+)
 
-# Define the index settings and mappings
-index_settings = {
-    'settings': {
-        'number_of_shards': 1,
-        'number_of_replicas': 0
-    },
-    'mappings': {
-        'properties': {
-            'first_name': {'type': 'text'},
-            'last_name': {'type': 'text'},
-            'email': {'type': 'text'},
-            'profile_picture': {'type': 'keyword'},
-            'role': {'type': 'text'},
-            'qr_code': {'type': 'keyword'},
-            'tribe': {'type': 'text'},
-            'company_address': {'type': 'nested'},
-            'phone_number': {'type': 'text'},
-            'card_type' : {'type': 'text'},
-            'is_active': {'type': 'boolean'},
-            'is_deleted': {'type': 'boolean'},
-            'created_date': {'type': 'date'},
-            'updated_date': {'type': 'date'}
-        }
-    }
-}
-
-# Create the index
-response = es.indices.create(index='cards', body=index_settings)
-
-# Check if the index creation was successful
-if response['acknowledged']:
-    print(f"The index cards_index was created successfully.")
-else:
-    print(f"Failed to create the index 'cards_index'.")
+# Get a list of project indexes
+response = client.indices.get_alias(index="myproject_*")
+print('heyyy')
+indexes = list(response.keys())
+print(indexes)
+# Delete each index
+for index in indexes:
+    response = client.indices.delete(index=index, ignore=[400, 404])
+    if response['acknowledged']:
+        print(f"The index {index} was deleted successfully.")
+    else:
+        print(f"Failed to delete the index {index}.")
--- card/signals.py
+++ card/signals.py
@@ -38,33 +38,33 @@ def log_user_login_failed(sender, credentials, request, **kwargs):
 
 
 
-from django.db.models.signals import post_save, post_delete
-from django.dispatch import receiver
+# from django.db.models.signals import post_save, post_delete
+# from django.dispatch import receiver
 
-from django_elasticsearch_dsl.registries import registry
-
-
-@receiver(post_save)
-def update_document(sender, **kwargs):
-    app_label = sender._meta.app_label
-    model_name = sender._meta.model_name
-    instance = kwargs['instance']
-
-    if app_label == 'card':
-        if model_name == 'Expertcard':
-            instances = instance.article.all()
-            for _instance in instances:
-                registry.update(_instance)
+# from django_elasticsearch_dsl.registries import registry
 
 
-@receiver(post_delete)
-def delete_document(sender, **kwargs):
-    app_label = sender._meta.app_label
-    model_name = sender._meta.model_name
-    instance = kwargs['instance']
+# @receiver(post_save)
+# def update_document(sender, **kwargs):
+#     app_label = sender._meta.app_label
+#     model_name = sender._meta.model_name
+#     instance = kwargs['instance']
 
-    if app_label == 'card':
-        if model_name == 'Expertcard':
-            instances = instance.article.all()
-            for _instance in instances:
-                registry.update(_instance)
+#     if app_label == 'card':
+#         if model_name == 'Expertcard':
+#             instances = instance.article.all()
+#             for _instance in instances:
+#                 registry.update(_instance)
+
+
+# @receiver(post_delete)
+# def delete_document(sender, **kwargs):
+#     app_label = sender._meta.app_label
+#     model_name = sender._meta.model_name
+#     instance = kwargs['instance']
+
+#     if app_label == 'card':
+#         if model_name == 'Expertcard':
+#             instances = instance.article.all()
+#             for _instance in instances:
+#                 registry.update(_instance)
